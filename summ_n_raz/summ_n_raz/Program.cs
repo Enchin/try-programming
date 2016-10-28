bb1e1da1ddb08b434b84c2342cd634d104acca98
donen <don.encoder11@gmail.com>
2016-10-28 21:39:59 +0300

    done

---
+++ summ_n_raz/summ_n_raz/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace summ_n_raz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("От от 1 до скольки суммировать");
+            string a = Console.ReadLine();
+            int c = int.Parse(a);
+            int rezultat = 1;
+            int zero = 1;
+            while (zero <= c)
+            {
+                rezultat = rezultat + zero;
+                zero = zero + 1;
+            }
+            Console.WriteLine(rezultat);
+            Console.ReadLine();
+        }
+    }
+}
